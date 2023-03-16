b8d1e49f413d83cc3a42b2c64daf790b6c65e937
omkar1204 <omahirkar008@gmail.com>
2023-03-16 17:16:23 +0530

    File Handling

---
+++ src/main/java/com/bootcoding/fileHandling/FileRead.java
@@ -0,0 +1,24 @@
+package com.bootcoding.fileHandling;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileRead {
+    public static void main(String[] args) {
+
+                try {
+                    File file = new File("D://BOOTCODING//file-processing//firstone//firstone.txt");
+                    Scanner myReader = new Scanner(file);
+                    while (myReader.hasNextLine()) {
+                        String data = myReader.nextLine();
+                        System.out.println(data);
+                    }
+                    myReader.close();
+                } catch (FileNotFoundException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+            }
+        }
+
