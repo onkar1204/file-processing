f71904f438b15e57afee0891880de42d49e0d33d
onkar1204 <114867129+onkar1204@users.noreply.github.com>
2023-04-07 14:26:26 +0530

    Update FileRead.java

--- src/main/java/com/bootcoding/fileHandling/FileRead.java
+++ src/main/java/com/bootcoding/fileHandling/FileRead.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class FileRead {
   
     public static void main(String[] args) {
-    
+   
                 try {
                     File file = new File("D://BOOTCODING//file-processing//firstone//firstone.txt");
                     Scanner myReader = new Scanner(file);
